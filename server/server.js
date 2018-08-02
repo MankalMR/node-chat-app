0cee6f38d2076a48efcf0043757871876a293186
Manohar R Mankala <manohar.mankala@gmail.com>
2018-08-02 16:24:25 -0400

    styling the look and feel of chat app

--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const express = require('express');
-const fs = require('fs');
 const socketIO = require('socket.io');
 const http = require('http');
 
@@ -10,13 +9,44 @@ const port = process.env.PORT || 3000;
 const server = http.createServer(app);
 const io = socketIO(server);
 
-app.get('/', (req, res) => {
-    res.sendFile(publicPath + '/index.html');
-});
-// app.use(express.static(publicPath));
+// app.get('/', (req, res) => {
+//     res.sendFile(publicPath + '/index.html');
+// });
+app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
   console.log('New User Connected!');
+
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the Chat App!',
+    createdAt: new Date().toDateString()
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New User Joined!',
+    createdAt: new Date().toDateString()
+  });
+
+  socket.on('createMessage', (message, callback) => {
+    console.log('created Message!', message);
+    // This is used to emit to everyone connected.
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().toDateString()
+    });
+
+    // This is used to emit to everyone connected but the user creating the message.
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().toDateString()
+    // });
+
+    callback();
+  });
 
   socket.on('disconnect', () => {
     console.log('User Disconnected!');
